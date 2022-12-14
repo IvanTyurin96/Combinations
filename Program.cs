9ab42a00b94aaaa30206a9e8444f306fd91071a1
IvanTyurin96 <88649748+IvanTyurin96@users.noreply.github.com>
2022-12-14 23:56:14 +0300

    Начальный коммит

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArray = new []{'A','B','C','D'};
+
+            IEnumerable<String> variants = GetAllSequences(charArray);
+            
+            foreach(string variant in variants)
+            {
+                Console.WriteLine(variant);
+            }
+        }
+
+        static IEnumerable<String> GetAllSequences(IEnumerable<char> chars)
+        {
+            if(chars.Count() == 1)
+                return chars.Select(ch => ch.ToString());
+
+            return chars.SelectMany((ch, index) => 
+                                    GetAllSequences(chars.RemoveAt(index))
+                                    .Select(str => $"{ch}{str}")); 
+        }
+    }
+
+    static class Extensions
+    {
+        //Данный метод убирает символ с определенным индексом из последовательности
+        //и возвращает новую последовательность без этого символа.
+        public static IEnumerable<T> RemoveAt<T>(this IEnumerable<T> sequence, int index)
+        {
+            return sequence.Take(index).Concat(sequence.Skip(index + 1));
+        }
+    }
+}
